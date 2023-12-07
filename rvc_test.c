a5fa57c860abd7569b9d8b954f4f7c0256dbfacc
chans1128 <happyshin99@naver.com>
2023-12-07 20:05:45 +0900

    [CHORE] 테스트 케이스 별 주석 추가

--- rvc_test.c
+++ rvc_test.c
@@ -3,51 +3,48 @@
 #include <stdlib.h>
 #include "rvc.h"
 
-//1
+// Test case 1.1
 void test_FrontSensorInterface_NoObstacle(void) {
 	FrontSensorInput = false;
 	CU_ASSERT_FALSE(FrontSensorInterface());
 }
-
+// Test case 1.2
 void test_FrontSensorInterface_ObstaclePresent(void) {
 	FrontSensorInput = true;
 	CU_ASSERT_TRUE(FrontSensorInterface());
 }
-
-//2
+// Test case 1.3
 void test_RightSensorInterface_NoObstacle(void) {
 	RightSensorInput = false;
 	CU_ASSERT_FALSE(RightSensorInterface());
 }
-
+// Test case 1.4
 void test_RightSensorInterface_ObstaclePresent(void) {
 	RightSensorInput = true;
 	CU_ASSERT_TRUE(RightSensorInterface());
 }
-
-//3
+// Test case 1.5
 void test_LeftSensorInterface_NoObstacle(void) {
 	LeftSensorInput = false;
 	CU_ASSERT_FALSE(LeftSensorInterface());
 }
-
+// Test case 1.6
 void test_LeftSensorInterface_ObstaclePresent(void) {
 	LeftSensorInput = true;
 	CU_ASSERT_TRUE(LeftSensorInterface());
 }
-
-//4
+// Test case 1.7
 void test_DustSensorInterface_NoDust(void) {
 	DustSensorInput = false;
 	CU_ASSERT_FALSE(DustSensorInterface());
 }
-
+// Test case 1.8
 void test_DustSensorInterface_DustPresent(void) {
 	DustSensorInput = true;
 	CU_ASSERT_TRUE(DustSensorInterface());
 }
 
-//5
+// Test case 2.1
 void test_DetermineObstacleLocation(void) {
 	FrontSensorInput = true;
 	LeftSensorInput = true;
@@ -63,94 +60,90 @@ void test_DetermineObstacleLocation(void) {
 	free(result);
 }
 
-//6
+// Test case 3.1
 void test_DetermineDustExistence_true(void) {
 	DustSensorInput = true;
 	CU_ASSERT_TRUE(DetermineDustExistence());
 }
-
+// Test case 3.2
 void test_DetermineDustExistence_false(void) {
 	DustSensorInput = false;
 	CU_ASSERT_FALSE(DetermineDustExistence());
 }
 
-//7
+// Test case 4.1
 void test_MoveForward_enable(void) {
 	CU_ASSERT_EQUAL(MoveForward(true), 'F');
 }
-
+// Test case 4.2
 void test_MoveForward_disable(void) {
 	CU_ASSERT_EQUAL(MoveForward(false), 'S');
 }
-
-//8
+// Test case 4.3
 void test_TurnLeft(void) {
 	CU_ASSERT_EQUAL(TurnLeft(), 'L');
 }
-
-//9
+// Test case 4.4
 void test_TurnRight(void) {
 	CU_ASSERT_EQUAL(TurnRight(), 'R');
 }
-
-//10
+// Test case 4.5
 void test_TurnBack(void) {
 	CU_ASSERT_EQUAL(TurnBack(), 'B');
 }
 
-//11
+// Test case 5.1
 void test_PowerUp(void) {
 	CU_ASSERT_EQUAL(PowerUp(), 'U');
 }
 
-//12
+// Test case 6.1
 void test_Cleaner_On(void) {
 	CU_ASSERT_TRUE(Cleaner(true));
 }
-
+// Test case 6.2
 void test_Cleaner_Off(void) {
 	CU_ASSERT_TRUE(Cleaner(false));
 }
 
-// 13
+// Test case 7.1
 void test_CleanerInterface_TurnOn(void) {
 	CU_ASSERT_EQUAL(CleanerInterface('O'), 'O');
 }
-
+// Test case 7.2
 void test_CleanerInterface_TurnOff(void) {
 	CU_ASSERT_EQUAL(CleanerInterface('F'), 'F');
 }
-
+// Test case 7.3
 void test_CleanerInterface_PowerUp(void) {
 	CU_ASSERT_EQUAL(CleanerInterface('U'), 'U');
 }
-
+// Test case 7.4
 void test_CleanerInterface_Exception(void) {
 	CU_ASSERT_EQUAL(CleanerInterface('A'), 'E');
 }
-
-// 14
+// Test case 7.5
 void test_MotorInterface_MoveForward(void) {
 	CU_ASSERT_EQUAL(MotorInterface('F'), 'F');
 }
-
+// Test case 7.6
 void test_MotorInterface_TurnLeft(void) {
 	CU_ASSERT_EQUAL(MotorInterface('L'), 'L');
 }
-
+// Test case 7.7
 void test_MotorInterface_TurnRight(void) {
 	CU_ASSERT_EQUAL(MotorInterface('R'), 'R');
 }
-
+// Test case 7.8
 void test_MotorInterface_TurnBack(void) {
 	CU_ASSERT_EQUAL(MotorInterface('B'), 'B');
 }
-
+// Test case 7.9
 void test_MotorInterface_StopMotor(void) {
 	MotorInterface('S');
 	CU_ASSERT_EQUAL(MotorInterface('S'), 'S');
 }
-
+// Test case 7.10
 void test_MotorInterface_Exception(void) {
 	CU_ASSERT_EQUAL(MotorInterface('A'), 'E');
 }
@@ -203,8 +196,9 @@ int main() {
 	CU_add_test(suite7, "Test case 7.5", test_MotorInterface_MoveForward);
 	CU_add_test(suite7, "Test case 7.6", test_MotorInterface_TurnLeft);
 	CU_add_test(suite7, "Test case 7.7", test_MotorInterface_TurnRight);
-	CU_add_test(suite7, "Test case 7.8", test_MotorInterface_StopMotor);
-	CU_add_test(suite7, "Test case 7.9", test_MotorInterface_Exception);
+	CU_add_test(suite7, "Test case 7.8", test_MotorInterface_TurnBack);
+	CU_add_test(suite7, "Test case 7.9", test_MotorInterface_StopMotor);
+	CU_add_test(suite7, "Test case 7.10", test_MotorInterface_Exception);
 
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	CU_basic_run_tests();
